d0f4fa86131531d9d1cd57d2c0c7341f84154037
aundra miller and jared beckler <jared.beckler@gmail.com>
2016-02-10 09:12:02 -0800

    add final functionality to web app

--- src/Task.php
+++ src/Task.php
@@ -18,6 +18,20 @@ class Task
         return $this->description;
     }
 
+    function save()
+    {
+        array_push($_SESSION["list_of_tasks"], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_tasks'];
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['list_of_tasks'] = array();
+    }
 }
 
  ?>
